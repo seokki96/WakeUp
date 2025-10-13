0d82c26d6dc09d8e7cadd00570b5c24ece32210e
권석기 <kwons3327@gmail.com>
2025-10-13 23:46:33 +0900

    add: AlarmSettingView 추가

---
+++ WakeUp/Presentation/AlarmSetting/View/AlarmSettingView.swift
@@ -0,0 +1,18 @@
+//
+//  AlarmSettingView.swift
+//  WakeUp
+//
+//  Created by a on 10/13/25.
+//
+
+import SwiftUI
+
+struct AlarmSettingView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    AlarmSettingView()
+}
