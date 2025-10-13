b0d843a30d36e24fa16656c798b47f3816d781cf
권석기 <kwons3327@gmail.com>
2025-10-13 23:16:09 +0900

    chore: 폴더 분리

---
+++ WakeUp/Presentation/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  WakeUp
+//
+//  Created by a on 10/13/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    MainView()
+}
