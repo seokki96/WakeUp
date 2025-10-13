fb78302063544a39c3300e87c8f25f5fd40f8de4
권석기 <kwons3327@gmail.com>
2025-10-13 23:10:31 +0900

    Initial Commit

---
+++ WakeUp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WakeUp
+//
+//  Created by a on 10/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WakeUp/WakeUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeUpApp.swift
+//  WakeUp
+//
+//  Created by a on 10/13/25.
+//
+
+import SwiftUI
+
+@main
+struct WakeUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
